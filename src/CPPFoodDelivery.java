3412e62813e2d7fbaea8af31ec53a2ee6bc79cfe
Jake Terrey <jaketerrey@gmail.com>
2024-05-13 22:49:56 -0700

    Adding Singleton

---
+++ src/CPPFoodDelivery.java
@@ -0,0 +1,29 @@
+class CPPFoodDelivery {
+    private static CPPFoodDelivery instance;
+    private List<Customer> customers = new ArrayList<>();
+    private List<Restaurant> restaurants = new ArrayList<>();
+    private List<Driver> drivers = new ArrayList<>();
+
+    private CPPFoodDelivery() {
+    }
+
+    public static CPPFoodDelivery getInstance() {
+        if (instance == null) {
+            instance = new CPPFoodDelivery();
+        }
+        return instance;
+    }
+
+    public void registerCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public void registerRestaurant(Restaurant restaurant) {
+        restaurants.add(restaurant);
+    }
+
+    public void registerDriver(Driver driver) {
+        drivers.add(driver);
+    }
+
+}
