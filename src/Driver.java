558b4373daa05c6839fa8accd04b271bbd83ec26
JRSiwiecki <JRSiwiecki@gmail.com>
2024-05-14 16:12:13 -0700

    Add Driver constructor, update parameters to proper types

--- src/Driver.java
+++ src/Driver.java
@@ -2,8 +2,56 @@ public abstract class Driver
 {
     private String name;
     private String address;
-    private County operatingCounty;
-    private DriverTimeShift shift;
+    private County.Area operatingCounty;
+    private DriverTimeShift.Shift workShift;
+
+    public Driver(String name, String address, County.Area operatingCounty, DriverTimeShift.Shift workShift)
+    {
+        this.name = name;
+        this.address = address;
+        this.operatingCounty = operatingCounty;
+        this.workShift = workShift;
+    }
 
     abstract void deliverFood(Order order);
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public County.Area getOperatingCounty()
+    {
+        return operatingCounty;
+    }
+
+    public void setOperatingCounty(County.Area operatingCounty)
+    {
+        this.operatingCounty = operatingCounty;
+    }
+
+    public DriverTimeShift.Shift getWorkShift()
+    {
+        return workShift;
+    }
+
+    public void setWorkShift(DriverTimeShift.Shift workShift)
+    {
+        this.workShift = workShift;
+    }
 }
