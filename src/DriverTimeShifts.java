a7328efc86ea0a1d624e3e12c708901bf0a4f7ec
JRSiwiecki <JRSiwiecki@gmail.com>
2024-05-14 12:08:05 -0700

    Add DriverTimeShifts enum

---
+++ src/DriverTimeShifts.java
@@ -0,0 +1,22 @@
+public class DriverTimeShifts
+{
+    /**
+     * Represents the shift a driver begins working at.
+     */
+    enum Shifts {
+        /**
+         * 8AM - 4PM
+         */
+        MORNING,
+
+        /**
+         * 4PM - 12AM
+         */
+        AFTERNOON,
+
+        /**
+         * 12AM - 8AM
+         */
+        MIDNIGHT
+    }
+}
