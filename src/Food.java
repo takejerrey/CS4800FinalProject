126e4b602572b5d00a9246c0f9c35b32ac0e8092
JRSiwiecki <JRSiwiecki@gmail.com>
2024-05-17 12:15:32 -0700

    Rework food to be abstract

--- src/Food.java
+++ src/Food.java
@@ -1,4 +1,25 @@
-public interface Food {
-    double getPrice();
-    String getName();
+public abstract class Food {
+    private final double price;
+    private final String name;
+    private final Macronutrient carb;
+    private final Macronutrient protein;
+    private final Macronutrient fat;
+
+    public Food(double price, String name, Macronutrient carb, Macronutrient protein, Macronutrient fat) {
+        this.price = price;
+        this.name = name;
+        this.carb = carb;
+        this.protein = protein;
+        this.fat = fat;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
 } 
