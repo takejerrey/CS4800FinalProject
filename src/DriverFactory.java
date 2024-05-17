b8cfe9ff6cf27043a300f2926d0a72135103be73
JRSiwiecki <JRSiwiecki@gmail.com>
2024-05-17 16:00:13 -0700

    Add DriverFactoryTest and tests, fix bug with hours

--- src/DriverFactory.java
+++ src/DriverFactory.java
@@ -3,7 +3,7 @@ public class DriverFactory
     public static Driver getDriver(String name, String address,
                                    County.Area operatingCounty, int shiftStartHour, int shiftEndHour)
     {
-        if (shiftStartHour >= 8 && shiftStartHour <= 16)
+        if (shiftStartHour >= 8 && shiftStartHour < 16)
         {
             return new MorningDriver(name, address, operatingCounty, shiftStartHour, shiftEndHour);
         }
