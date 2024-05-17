4118c201d3b2b188c331f2a43a4f254d74cd275a
JRSiwiecki <JRSiwiecki@gmail.com>
2024-05-17 12:48:12 -0700

    Add more tests in main

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,22 @@ public class Main {
     {
         CPPFoodDelivery app = CPPFoodDelivery.getInstance();
 
-        Restaurant r1 = new Restaurant("R1", "001", County.Area.LA_COUNTY, 8, 20, "CT1");
-        r1.addMenuItem(BurgerFactory.getInstance().getFood(1.00, "Burger", DietaryRestriction.DietPlan.NO_RESTRICTION));
-        r1.addToppingItem("Burger", "Cheese");
-        app.registerRestaurant(r1);
+        app.registerRestaurant(new Chilis("Chili's", "001", County.Area.LA_COUNTY, 8, 20, "Tex-Mex"));
+        app.registerDriver(DriverFactory.getDriver("D1", "101", County.Area.LA_COUNTY, 8, 16));
+        app.registerCustomer(new Customer("C1", "201", County.Area.LA_COUNTY, DietaryRestriction.DietPlan.NO_RESTRICTION));
 
-        Driver d1 = DriverFactory.getDriver("D1", "101", County.Area.LA_COUNTY, 8, 16);
-        app.registerDriver(d1);
+        app.displayRegisteredRestaurants();
 
-        Customer c1 = new Customer("C1", "201", County.Area.LA_COUNTY, DietaryRestriction.DietPlan.NO_RESTRICTION);
-        app.registerCustomer(c1);
+        app.placeOrder("Chili's", "C1", new String[]{"Burger", "Cheese Burger"});
 
-        app.placeOrder(r1, c1, new String[]{"Burger", "Cheeseburger"});
+        app.registerDriver(DriverFactory.getDriver("D2", "102", County.Area.LA_COUNTY, 18, 0));
+        app.registerCustomer(new Customer("C2", "202", County.Area.LA_COUNTY, DietaryRestriction.DietPlan.PALEO));
+
+        SimulatedTime.getInstance().setTime(18, 0);
+
+        app.placeOrder("Chili's", "C2", new String[]{"Chicken Strips", "BBQ Chicken Strips", "Burger"});
+
+        SimulatedTime.getInstance().setTime(17, 0);
+        app.placeOrder("Chili's", "C2", new String[]{"Chicken Strips", "BBQ Chicken Strips", "Burger"});
     }
 }
