eb4f510687058d9b149bc18c25e0f6d1e6f63fd4
JRSiwiecki <JRSiwiecki@gmail.com>
2024-05-17 12:05:09 -0700

    Update Topping to have its own price

--- src/Topping.java
+++ src/Topping.java
@@ -1,15 +1,17 @@
 public abstract class Topping implements Food {
     private Food food;
     private String name;
+    private double toppingPrice;
     
-    public Topping(Food food, String name){ 
+    public Topping(Food food, String name, double toppingPrice){
         this.food = food;
         this.name = name;
+        this.toppingPrice = toppingPrice;
     } 
 
     @Override 
     public double getPrice() {
-        return food.getPrice();
+        return food.getPrice() + toppingPrice;
     }
 
     @Override
